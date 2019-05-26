e2c9fed535833c39c81598f99d6c453cf28ed1fc
lijiaocn <lijiaocn@foxmail.com>
2019-05-26 14:23:14 +0800

    m

--- gh-commit.sh
+++ gh-commit.sh
@@ -6,9 +6,10 @@
 # Distributed under terms of the GPL license.
 #
 
-cd ./_site
+git checkout gh-pages
+cp -rf _site/*  .
 sed -i "" -e "s#http://localhost:4000#https://www.lijiaocn.com#g" ./sitemap.xml
 sed -i "" -e "s#http://localhost:4000#https://www.lijiaocn.com#g" ./feed.xml
 git add .
-git commit -m "m"
+git commit -m "gh-page"
 git push 
